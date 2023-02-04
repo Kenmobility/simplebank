7d1ff4b5a453f233a02765f82d1f4c77851550b2
kenneth Amobi <kennmobility@gmail.com>
2023-02-04 01:42:55 +0100

    create custom params validators and to Gin

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 
 	db "github.com/kenmobility/simplebank/db/sqlc"
 )
@@ -17,6 +19,11 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 
 	router := gin.Default()
+
+	// register custom validators with Gin
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
 
 	router.POST("/account", server.createAccount)
 	router.GET("/account/:id", server.getAccount)
